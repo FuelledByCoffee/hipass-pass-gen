b8c80a9695e08664db5756865b59e045acbabcf4
Johannes Eckhoff <j.eckhoff@gmail.com>
2024-04-22 17:37:20 +0200

    Fix undefined unsigned alias
    Replace with 'unsigned'

--- src/rand.h
+++ src/rand.h
@@ -6,7 +6,7 @@ extern "C"
 #endif
 
 int random_nr(int min, int max);
-int random_wordpicker(uint wordcount, char separator);
+int random_wordpicker(unsigned wordcount, char separator);
 
 #ifdef __cplusplus
 }
